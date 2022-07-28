f699d0fbb38e781de8044911fbeafe4a502c1e89
Иван <i.sizov@vanguardsoft.ru>
2022-07-28 15:31:14 +0300

    #2 Донастройка проекта

--- src/test/kotlin/ii_tests_pim/Tools.kt
+++ src/test/kotlin/ii_tests_pim/Tools.kt
@@ -52,13 +52,8 @@ class Tools {
     }
 
     fun anyLogonTool(username: String, password: String){
-
         //https://overcoder.net/q/1369284/%D0%BA%D0%B0%D0%BA-%D1%80%D0%B0%D0%B7%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%B8%D0%BB%D0%B8-%D0%B7%D0%B0%D0%BF%D1%80%D0%B5%D1%82%D0%B8%D1%82%D1%8C-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BE-%D0%B2%D1%81%D0%BF%D0%BB%D1%8B%D0%B2%D0%B0%D1%8E%D1%89%D0%B5%D0%B9-%D0%BA%D0%B0%D0%BC%D0%B5%D1%80%D0%B5-%D0%BC%D0%B8%D0%BA%D1%80%D0%BE%D1%84%D0%BE%D0%BD%D0%B0
-
-
         //ChromeOptions addArguments
-
-
         //на случай невыполнения шага, ждем что бы можно было успеть глазками посмотреть и руками потыкать
         Configuration.timeout = 20000
         //выбираем браузер
