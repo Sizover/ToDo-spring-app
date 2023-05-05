84d90c33663235f6ff17a7f3914fd52ffc6c8e75
isizov <i.sizov@vanguardsoft.ru>
2023-05-05 17:31:45 +0300

    Первая попытка реализовать кастомизацию энвов, используя готовое решение из testng

---
+++ src/test/kotlin/Config.kt
@@ -0,0 +1,47 @@
+
+
+
+open class Config {
+//    var url = "https://test.kiap.local/"
+//    var mainLogin = "a.sizov"
+//    var mainPassword = "a.sizov"
+//    var adminLogin = "autotest_admin"
+//    var adminPassword = "autotest_admin"
+//
+//    init {
+//        System.getenv("TEST_URL")?.let {
+//            url = it
+//        }
+//        System.getenv("TEST_LOGIN")?.let {
+//            mainLogin = it
+//        }
+//        System.getenv("TEST_PASSWORD")?.let {
+//            mainPassword = it
+//        }
+//        System.getenv("TEST_ADMIN_LOGIN")?.let {
+//            adminLogin = it
+//        }
+//        System.getenv("TEST_ADMIN_PASSWORD")?.let {
+//            adminPassword = it
+//        }
+//
+//    }
+//
+//    companion object {
+//        var config: Config? = null
+//        fun getConfig1() = config ?: (Config().also { config = it })
+//    }
+//    @Parameters("url", "mainLogin", "mainPassword", "adminLogin", "adminPassword" )
+//    @BeforeMethod(alwaysRun = true)
+//    fun getConf(urlEnv: String?, mainLoginEnv: String?, mainPasswordEnv: String?, adminLoginEnv: String?, adminPasswordEnv: String?){
+//    val baseUrl = urlEnv
+//    val mainLogin = mainLoginEnv
+//    val mainPassword = mainPasswordEnv
+//    val adminLogin = adminLoginEnv
+//    val adminPassword = adminPasswordEnv
+//    }
+
+//    open fun beforeTest(env: String?) {
+//        baseUrl = env
+//    }
+}
